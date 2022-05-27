52e0c82486c5ed2d0949b5fa8b602b775d7f45df
Jordan Zdimirovic <jordan.zdimirovic@gmail.com>
2022-05-27 22:55:48 +1000

    OTA can now play a full string, with pauses between characters.

--- over_the_air.py
+++ over_the_air.py
@@ -15,21 +15,24 @@ class TonePlayer():
         A player object that can play tones.
     """
     def __init__(self, volume, freq):
-        self.pitch = 2 * log2(freq / 440) + 9
-        self.decib = 20 * log2(volume)
+        self.volume = volume
+        self.freq = freq
         self.playing = False
-        # Cap at 0 decibels
-        self.decib = 0 if self.decib > 0 else self.decib
-        
         pygame.mixer.init(44100,-16,2,512)
-
-        arr = np.array([4096 * np.sin(2.0 * np.pi * freq * x / 44100) for x in range(0, 44100)]).astype(np.int16)
+        self.generate_sound(volume, freq)
+        
+    def generate_sound(self, volume, freq):
+        arr = np.array([4096 * volume * np.sin(2.0 * np.pi * freq * x / 44100) for x in range(0, 44100)]).astype(np.int16)
         arr2 = np.c_[arr,arr]
         self.sound = pygame.sndarray.make_sound(arr2)
 
-    def set_pitch(self, new_pitch):
-        self.pitch = new_pitch
-        self.sw.set_pitch(self.pitch)
+    def set_freq(self, new_freq):
+        self.freq = new_freq
+        self.generate_sound(self.volume, new_freq)
+
+    def set_vol(self, new_vol):
+        self.freq = new_vol
+        self.generate_sound(new_vol, self.freq)
 
     def play_s(self, sec):
         self.sound_on()
@@ -80,6 +83,26 @@ class TonePlayer():
         )
 
 
+def bin2dec(v: str):
+    """Convert a binary number (as string) to decimal"""
+    assert all(l in "01" for l in v), "Binary string had digit that wasn't 0 or 1."
+    assert len(v) == 8, "Binary string must represent a single byte."
+    return sum(int(v[-(i+1)]) * (2**i) for i in range(8))
+
+def dec2bin(v: int):
+    """Convert a decimal number to binary number (as string)"""
+    assert 0 <= v <= 255, f"Value {v} must be within the size of a byte, and can't be negative."
+    b = ["0"] * 8
+    for i in range(8):
+        d = 2 ** (7-i)
+        if v >= d:
+            v -= d
+            b[i] = "1"
+    
+    assert v == 0, "Something went horribly wrong."
+
+    return "".join(b)
+
 class OTA():
     """
         Represents an over-the-air communications
@@ -87,14 +110,52 @@ class OTA():
     """
     def __init__(self, bit_length, byte_sep):
         """Constructor for OTA object."""
-        pass
+        self.bit_length = bit_length
+        self.byte_sep = byte_sep
+
+        self.tone_player = TonePlayer(1, 440)
+
+    def get_bit_play_instructions(self, bitstr, padded = True) -> list:
+        padding = [sound_on(self.bit_length)] if padded else []
+
+        return padding + [
+            sound_on(self.bit_length) if int(v) else sound_off(self.bit_length)
+            for v in bitstr
+        ] + padding
+
+    def emit_bits(self, bitstr, padded = True):
+        """
+            Plays a series of bits using the sound system and
+            the bit length option provided.
+            Pads each end with ON by default so that it can be understood
+        """
+        sound_instructions = self.get_bit_play_instructions(bitstr, padded)
+        self.tone_player.play_instructions(*sound_instructions)
+
+    def emit_byte_series(self, lst_bitstrs):
+        """
+            Plays a series of bytes
+        """
+        assert all(len(x) == 8 and all(c in "01" for c in x) for x in lst_bitstrs), "All sequences must be valid bytes."
+        sound_instructions = []
+        for bitstr in lst_bitstrs:
+            sound_instructions.extend(self.get_bit_play_instructions(bitstr, True) + [sound_off(self.byte_sep)])
+
+        self.tone_player.play_instructions(*sound_instructions)
+        
+    def emit_string(self, string):
+        """Plays a string by emiting each character as a byte"""
+        charbytes = list(map(dec2bin, [ord(c) for c in string]))
+        self.emit_byte_series(charbytes)
 
     def config_instructions(self):
         """Encode config info to sound and play it."""
-        pass
+        # First, emit 10 alternating bits, twice
+        self.emit_bits("10" * 10, padded = False)
 
+# --- TESTING ---
 
-def main():
+def test_timing():
     from timeit import default_timer
     # Create sound player
     # Test tone length
@@ -122,7 +183,15 @@ def main():
     print(f"Actual: {ttaken} seconds.")
     print(f"Diff: {abs(ttlen - ttaken)} seconds.")
     print(f"Ratio: {abs(ttlen - ttaken)/ttlen}%.")
-    
+
+def test_byte_emission():
+    ota = OTA(0.08, 1)
+    ota.emit_string("a")
+
+def main():
+    test_timing()
+    test_byte_emission()
+    time.sleep(1)
     
 
 if __name__ == '__main__': main()
